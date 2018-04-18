18c083825f17c55dd8938d9b978c9d54affd0eaa
fedcalmus <fedcalmus@gmail.com>
2018-04-18 19:01:47 +0400

    added js for tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-/**
- * Created by STDev031 on 4/13/2018.
- */
+$(function() {
+
+    var AppPlugins = {
+
+
+        tabs: function () {
+
+            $('.categoryItems li').click(function () {
+                var tab_id = $(this).attr('data-tab');
+
+                $('.categoryItems li').removeClass('current');
+                $('.grid').removeClass('current');
+                $(this).addClass('current');
+                $("#" + tab_id).addClass('current');
+            })
+        }
+    };
+
+
+    /* ------ Plugins Init ------ */
+
+    AppPlugins.tabs();
+
+});
